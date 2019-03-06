9f7d86e2f932ddb4f9e38766d6b44e99fa334f98
Travis CI User <travis@example.org>
2019-03-06 17:19:50 +0000

    Documentation for jameshurst/rust-id3@f797c6a

---
+++ implementors/core/ops/deref/trait.DerefMut.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["darling_core"] = [{text:"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"darling_core/util/struct.SpannedValue.html\" title=\"struct darling_core::util::SpannedValue\">SpannedValue</a>&lt;T&gt;",synthetic:false,types:["darling_core::util::spanned_value::SpannedValue"]},];
+implementors["regex_syntax"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.DerefMut.html\" title=\"trait core::ops::deref::DerefMut\">DerefMut</a> for <a class=\"struct\" href=\"regex_syntax/hir/literal/struct.Literal.html\" title=\"struct regex_syntax::hir::literal::Literal\">Literal</a>",synthetic:false,types:["regex_syntax::hir::literal::Literal"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
